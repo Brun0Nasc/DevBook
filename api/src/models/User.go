ec5c095db803c6c699935d91c147fe6edcb73dfd
Brun0.Nasc <brunonb114@gmail.com>
2024-07-31 13:34:36 -0300

    Creating user validations on POST

--- api/src/models/User.go
+++ api/src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User represents a user
 type User struct {
@@ -10,4 +14,40 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+// Prepare will format and validate the user data
+func (user *User) Prepare() (err error){
+	if err = user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return
+}
+
+func (user *User) validate() (err error) {
+	if user.Name == "" {
+		return errors.New("the field name is required")
+	}
+
+	if user.Nick == "" {
+		return errors.New("the field nick is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("the field email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("the field password is required")
+	}
+
+	return
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
