bcb8de0f6d248c65714248bf5275a9a8a8ac19f3
Brun0Nasc <brunonb114@gmail.com>
2025-04-05 21:11:47 -0300

    fix: fixing login issue

--- webapp/src/responses/responses.go
+++ webapp/src/responses/responses.go
@@ -16,10 +16,8 @@ func JSON(w http.ResponseWriter, statusCode int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if data != nil {
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			log.Fatal(err)
-		}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
 	}
 
 }
